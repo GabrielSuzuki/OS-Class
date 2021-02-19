287440436dfee9925a5c6a25eaf99e18948ed445
GabrielSuzuki <60160196+GabrielSuzuki@users.noreply.github.com>
2021-02-18 18:11:25 -0800

    updates comments

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -22,16 +22,6 @@ int main()
 
 	ifstream in("urls.txt");
 	string urlstring;
-	/*if (in.is_open())
-  {
-	while ( getline (in,urlstring) )
-	{
-	  //cout << urlstring << '\n';
-			++numLines;
-	}
-	in.close();
-  }*/
-
 
 	do {
 
@@ -44,53 +34,35 @@ int main()
 			/* TODO: Create a child */
 			if (in.is_open())
 			{
-				while (getline(in, urlstring) && pid > 0)
+				while (getline(in, urlstring) && pid > 0)//read each line
 				{
-					//cout << urlstring << '\n';
-					pid = fork();
+					pid = fork();//fork a child
 					cout << "forking child process: pid = " << pid << endl;
 					if (pid < 0) {//error occured
 						fprintf(stderr, "Fork Failed");
 						return 1;
 					}
 					else if (pid == 0) {//child process
-							  //cout << urlstring;
 
-						execlp("/usr/bin/wget", "wget", urlstring.c_str(), NULL);
+						execlp("/usr/bin/wget", "wget", urlstring.c_str(), NULL);//download url
 						cout << "Child Complete";
 					}
-					/*** TODO: If I am child, I will do this: ****/
-					/* Call execlp() to replace my program with that specified at the command line.
-					 * PLEASE NOTE: YOU CANNOT PASS cmdBuff DIRECTLY to execlp(). It is because
-					 * cmdBuff is an object of type string (i.e., a class) and execlp() expects
-					 * an array of characters.  However, you can pass cmdBuff.c_str(), which will
-					 * return an array of characters representation of the string object.
-					 *
-					 * Also, please do not forget to error check your exelp() system calls.
-					 */
 					else {//parent process
 						numLines++;
 						cout << "numLines = " << numLines << endl;
 					}
 				}
 
-				in.close();
-				if (pid > 0) {
+				in.close();//close file
+				if (pid > 0) {//wait numLines times
 					for (int i = 0; i < numLines; i++) {
 						wait(NULL);
 					}
 				}
 			}
 			else cout << "Unable to open file";
-
-
 		}
-
-		/* TODO: Error check to make sure the child was successfully created */
-
 	}
-	/*** TODO: If I am a parent, I will do the following ***?
-	/* Wait for the child process to terminate */
 	while (cmdBuff != "exit" && pid != 0);
 	return 0;
 }
